939ba6c26577d2b4a3ca388eb7d1a18ab08f211d
thenameiswiiwin <huyn.nguyen95@gmail.com>
2021-02-11 00:07:11 -0500

    js: displaying num-string on screen

--- script/index.js
+++ script/index.js
@@ -1,1 +1,36 @@
-console.log('Test');
+let runningTotal = 0;
+let buffer = '0';
+let previousOperator;
+
+const screen = document.querySelector('.screen');
+
+function buttonClick(value) {
+	if (isNaN(value)) {
+		// this is not a number
+		handleSymbol(value);
+	} else {
+		// this is a number
+		handleNumber(value);
+	}
+}
+
+function handleSymbol(symbol) {}
+
+function handleNumber(numberString) {
+	if (buffer === '0') {
+		buffer = numberString;
+	} else {
+		buffer += numberString;
+	}
+	screen.innerText = buffer;
+}
+
+function init() {
+	document
+		.querySelector('.calc-btns')
+		.addEventListener('click', function (event) {
+			buttonClick(event.target.innerText);
+		});
+}
+
+init();
